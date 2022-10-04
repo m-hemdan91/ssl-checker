1b1682e83fbf21fa4b12444b5e03519300c43146
marwa <marwa.kassem@ibtikar.net.sa>
2022-10-04 12:01:37 +0300

    initial commit

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 date_2day=`date +"%Y-%m-%d"`
-webhook_url=https://hooks.slack.com/services/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
+#webhook_url=https://hooks.slack.com/services/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
+webhook_url=
 
 while read -r domain ; do
+	[[ "$domain" =~ ^#.*$ ]] && continue
 	echo | openssl s_client -servername $domain -connect $domain:443 | openssl x509 -noout -dates | grep notAfter | awk 'BEGIN { FS = "=" } ; { print $2 }' > date_curl
         while read -r date_curl ; do
                 date_cert=`LC_ALL=C date -d "$date_curl" +'%Y-%m-%d'`
